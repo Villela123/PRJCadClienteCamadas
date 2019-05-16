2db5caea24eb275d9c62df43dd24519387792ad0
Villela123 <viniciusvillela92@gmail.com>
2019-05-16 14:28:45 -0300

    16052019 Pesquisa de todos feita

--- src/com/prjcadcliente/persistencia/CRUDCliente.java
+++ src/com/prjcadcliente/persistencia/CRUDCliente.java
@@ -189,6 +189,41 @@ public class CRUDCliente {
 		return null;
 	}
 	public List<Cliente> PesquisarTodos(){
-		return null;
+		List<Cliente> lista = new ArrayList<Cliente>();
+		try{
+		//carregar o drive de comunicacao com o banco de dados
+			Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+		//chamar o gerenciador de driver
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3307/clienteedb","root","");
+		//vamos criar a consulta para selecionar os clientes por nome
+			String consulta = "Select * from tbcliente";
+			pst = con.prepareStatement(consulta);
+		//vamos executar a consulta e guardar o resultado na variável rs
+			rs = pst.executeQuery();
+		//vamos pegar um cliente por vez que esta no rs e adiciona-lo à lista de clientes para, então, retorna-la
+			while(rs.next()){
+				lista.add(new Cliente(
+						rs.getInt(0),
+						rs.getString(1),
+						rs.getString(2),
+						rs.getString(3),
+						rs.getInt(4)
+						));
+			}//fim do while
+		}//fim do try
+		catch (SQLException ex){		
+			ex.printStackTrace();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+		finally{
+			try{con.close();
+		}
+		catch(Exception e){
+			e.printStackTrace();
+		}
+	    }
+		return lista;
 	}
 }
